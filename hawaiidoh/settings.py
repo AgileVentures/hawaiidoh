54672d804a965c45b9ba599401f4a00a91726f0c
Thomas Wassum <thomas.e.wassum@gmail.com>
2015-12-05 23:34:14 -1000

    forgot to add template directory

--- hawaiidoh/settings.py
+++ hawaiidoh/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'hawaiidoh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PACKAGE_ROOT, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
